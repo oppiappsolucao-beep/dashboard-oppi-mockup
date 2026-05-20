7ee52ad92c31ec1534496ad2446ce5597fbbfe6d
oppiappsolucao-beep <oppiappsolucao@gmail.com>
2026-05-20 19:03:46 -0300

    Create app.py

---
+++ app.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Oppi Dashboard",
+    layout="wide",
+    initial_sidebar_state="collapsed"
+)
+
+st.title("Dashboard Comercial Oppi")
+
+st.write("Sistema mockup para apresentação comercial.")
